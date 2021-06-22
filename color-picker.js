dc7ca41491c3ef237acb29b5049d6ce3da97d34d
Benny Powers <web@bennypowers.com>
2021-06-22 15:11:09 +0300

    step 5: apply controller-host-mixin

--- color-picker.js
+++ color-picker.js
@@ -1,15 +1,21 @@
+import { ControllerHostMixin } from 'https://unpkg.com/@apollo-elements/mixins@next/controller-host-mixin.js?module';
+
 const template = document.createElement('template');
       template.innerHTML = `
         <link rel="stylesheet" href="color-picker.css">
         <div id="loupe"></div>
       `;
 
-class ColorPicker extends HTMLElement {
+class ColorPicker extends ControllerHostMixin(HTMLElement) {
   constructor() {
     super()
     this
       .attachShadow({ mode: 'open' })
       .append(template.content.cloneNode(true));
+  }
+
+  update() {
+    super.update();
   }
 }
 
